539fbe0f0c0442728aacc2bfdd4ea1d513fb65ec
muslim <daiyanmuslim4@gmail.com>
2026-05-22 11:30:00 +0700

    feat: add poin-positif and poin-negatif list pages

---
+++ app/(admin)/master/poin-negatif/page.tsx
@@ -0,0 +1,10 @@
+import { PoinPage } from "../_poin/poin-page";
+import type { SearchParams } from "@/lib/list-params";
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  return <PoinPage tipe="NEGATIF" searchParams={searchParams} />;
+}
