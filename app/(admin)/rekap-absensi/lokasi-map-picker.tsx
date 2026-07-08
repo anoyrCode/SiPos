05f6f2e52d9274500aa6f94b86a3cc33263d7d87
muslim <daiyanmuslim4@gmail.com>
2026-07-08 13:41:10 +0700

    fix: hapus subdomain {s} lama di URL tile OpenStreetMap (sudah tidak dipakai OSM)

--- app/(admin)/rekap-absensi/lokasi-map-picker.tsx
+++ app/(admin)/rekap-absensi/lokasi-map-picker.tsx
@@ -60,7 +60,7 @@ export default function LokasiMapPicker({
       <MapContainer center={center} zoom={zoom} className="h-full w-full">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker
           position={[safeLat, safeLong]}
